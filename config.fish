2e0ab78786675a3905a922bcfb8a04b2b9f95d41
Dan Motzenbecker <dan@oxism.com>
2013-07-30 20:24:09 -0400

    basic config

---
+++ config.fish
@@ -0,0 +1,7 @@
+# environment
+set    fish_greeting ''
+set    HISTSIZE      20000
+set -x EDITOR        subl
+set -x PATH          $PATH /usr/local/share/npm/bin
+set -x NODE_ENV      'development'
+set -x NODE_PATH     '/usr/local/lib/node'
