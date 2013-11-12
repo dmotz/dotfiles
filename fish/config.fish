d09a902f2a520982f1f8daa79e5e901a5b2d289a
Dan Motzenbecker <dan@oxism.com>
2013-11-12 01:52:00 -0500

    moved aliases

--- fish/config.fish
+++ fish/config.fish
@@ -8,18 +8,7 @@ set -x NODE_PATH     '/usr/local/lib/node'
 
 
 # aliases
-alias g       'grep -i'
-alias lll     'll -a'
-alias coff    'coffee -mwc .'
-alias canary  'open -a Google\ Chrome\ Canary --args --disable-web-security'
-alias hg      'history | g'
-alias snoop   'sudo opensnoop -p'
-alias fhere   'mfind -onlyin .'
-alias fname   'mfind -name'
-alias stylus  'stylus -u nib'
-alias ports   'lsof -i | g listen'
-alias nosleep 'pmset noidle'
-
+. ~/.config/fish/aliases.fish
 
 # bookmarks
 . ~/.config/fish/bookmarks.fish
