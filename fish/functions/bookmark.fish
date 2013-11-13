8ec2d18542b0ca58f777b7e51d250db65197a530
Dan Motzenbecker <dan@oxism.com>
2013-11-12 20:16:39 -0500

    killed a space

--- fish/functions/bookmark.fish
+++ fish/functions/bookmark.fish
@@ -1,4 +1,4 @@
 function bookmark
-  echo 'alias' $argv \''cd' (eval pwd)\'  >> ~/.config/fish/bookmarks.fish
+  echo 'alias' $argv \''cd' (eval pwd)\' >> ~/.config/fish/bookmarks.fish
   . ~/.config/fish/bookmarks.fish
 end
