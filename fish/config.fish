379bb9dd22c6224aab599877785515bad8ac2b63
Dan Motzenbecker <dan@oxism.com>
2013-11-12 20:15:27 -0500

    removed some comments

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,3 @@
-# environment
 set    fish_greeting ''
 set    HISTSIZE      20000
 set -x EDITOR        subl
@@ -6,9 +5,5 @@ set -x PATH          $PATH /usr/local/share/npm/bin
 set -x NODE_ENV      'development'
 set -x NODE_PATH     '/usr/local/lib/node'
 
-
-# aliases
 . ~/.config/fish/aliases.fish
-
-# bookmarks
 . ~/.config/fish/bookmarks.fish
