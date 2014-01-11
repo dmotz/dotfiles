619d57357db00bfe7beabb0463df96d39409c821
Dan Motzenbecker <dan@oxism.com>
2014-01-11 00:50:06 -0500

    boiler fn

---
+++ fish/functions/boiler.fish
@@ -0,0 +1,19 @@
+function boiler
+  echo '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+  <meta name="author" content="Dan Motzenbecker">
+  <!-- <link href="images/favicon.png" rel="icon" type="image/png"> -->
+  <!-- <link rel="stylesheet" type="text/css"> -->
+  <title></title>
+</head>
+<body>
+  <script src=""></script>
+</body>
+</html>
+'
+end
