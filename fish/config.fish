8455cdaf1b5a364a58455f8bb8ca05324f2a6a8e
Dan Motzenbecker <dan@oxism.com>
2014-01-11 00:39:59 -0500

    color grep

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,8 @@ set -x EDITOR        subl
 set -x PATH          $PATH /usr/local/share/npm/bin
 set -x NODE_ENV      'development'
 set -x NODE_PATH     '/usr/local/lib/node'
+set -x GREP_OPTIONS  '--color=always'
+set -x GREP_COLOR    '1;35;40'
 
 . ~/.config/fish/aliases.fish
 . ~/.config/fish/bookmarks.fish
