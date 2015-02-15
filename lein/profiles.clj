36879b2b2f220748a712d7e515a7290f195b2c60
Dan Motzenbecker <dan@oxism.com>
2015-02-15 17:27:08 -0500

    lein plugins

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,2 +1,8 @@
-{:user {:plugins [[lein-ancient "0.6.2"] [venantius/ultra "0.2.0"]]
-        :ultra {:color-scheme :solarized_dark}}}
+{:user {
+    :plugins [
+        [lein-ancient "0.6.2"]
+        [venantius/ultra "0.2.0"]
+        [lein-cljfmt "0.1.9"]
+        [jonase/eastwood "0.2.1"]
+        [lein-bikeshed "0.2.0"]]
+    :ultra {:color-scheme :solarized_dark}}}
