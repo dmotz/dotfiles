c2947f49cc314dfd4b5fb6a732204d0ca0026573
Dan Motzenbecker <dan@oxism.com>
2015-11-02 21:38:03 -0500

    added lein-try

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,8 +1,11 @@
 {:user {
-    :plugins [
-        [lein-ancient "0.6.7"]
-        [venantius/ultra "0.3.4"]
-        [lein-cljfmt "0.1.10"]
-        [jonase/eastwood "0.2.1"]
-        [lein-bikeshed "0.2.0"]]
-    :ultra {:color-scheme :solarized_dark}}}
+  :plugins [
+      [lein-ancient "0.6.7"]
+      [venantius/ultra "0.3.4"]
+      [lein-cljfmt "0.1.10"]
+      [jonase/eastwood "0.2.1"]
+      [lein-bikeshed "0.2.0"]
+      [lein-try "0.4.3"]]
+
+  :ultra {
+    :color-scheme :solarized_dark}}}
