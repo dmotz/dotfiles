54d5bf9b49f3e2ba7ae2fd82749071a0d1cfaaeb
Dan Motzenbecker <dan@oxism.com>
2015-11-11 20:19:47 -0500

    handle lein with no args

--- fish/functions/lein.fish
+++ fish/functions/lein.fish
@@ -1,10 +1,14 @@
 function lein
-  switch $argv[1]
-    case repl figwheel
-      command rlwrap lein $argv
-    case ancient
-      command lein $argv :all
-    case '*'
-      command lein $argv
+  if test (count $argv) -eq 0
+    command lein
+  else
+    switch $argv[1]
+      case repl figwheel
+        command rlwrap lein $argv
+      case ancient
+        command lein $argv :all
+      case '*'
+        command lein $argv
+    end
   end
 end
