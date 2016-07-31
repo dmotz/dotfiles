92c4a531fbe3d1076a6e07ee11eb5566c7bf2e2e
Dan Motzenbecker <dan@oxism.com>
2016-07-30 20:25:26 -0400

    cleaned up path

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 set    fish_greeting ''
 set    HISTSIZE      20000
 set -x EDITOR        atom
-set -x PATH          $PATH /usr/local/bin /usr/local/share/npm/bin /usr/local/opt/ruby/bin ~/.cabal/bin ~/.rvm/bin
+set -x PATH          $PATH /usr/local/bin /usr/local/opt/ruby/bin ~/.rvm/bin
 set -x NODE_ENV      'development'
 set -x NODE_PATH     '/usr/local/lib/node'
 set -x GREP_OPTIONS  '--color=always'
