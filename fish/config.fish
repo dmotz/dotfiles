002989cd544e2d2415a7ce6b503a9438edd83f3f
Dan Motzenbecker <dan@oxism.com>
2016-08-01 23:37:57 -0400

    cuda vars

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,14 @@
 set    fish_greeting ''
 set    HISTSIZE      20000
 set -x EDITOR        atom
-set -x PATH          $PATH /usr/local/bin /usr/local/opt/ruby/bin ~/.rvm/bin
+set -x PATH          $PATH /usr/local/bin /usr/local/opt/ruby/bin ~/.rvm/bin /usr/local/cuda/bin
 set -x NODE_ENV      'development'
 set -x NODE_PATH     '/usr/local/lib/node'
 set -x GREP_OPTIONS  '--color=always'
 set -x GREP_COLOR    '1;35;40'
+
+set -x CUDA_HOME         /usr/local/cuda
+set -x DYLD_LIBRARY_PATH $DYLD_LIBRARY_PATH $CUDA_HOME/lib
 
 . ~/.config/fish/aliases.fish
 . ~/.config/fish/bookmarks.fish
