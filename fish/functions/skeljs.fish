09c5e354c806d4cb41b07b265f0f90b4570d5941
Dan Motzenbecker <dan@oxism.com>
2016-11-27 17:23:30 -0500

    js skeleton script

---
+++ fish/functions/skeljs.fish
@@ -0,0 +1,11 @@
+function skeljs
+  sed 's/\$\$\$/'(basename (pwd))'/' < (dirname (status -f))'/skeletons/js/package.json' > package.json
+  git init
+  mkdir src
+  touch src/index.js
+  mkdir style
+  touch style/main.styl
+  atom
+  npm i -S budo babelify browserify babel-preset-es2015 parallelshell stylus nib
+  npm run dev
+end
