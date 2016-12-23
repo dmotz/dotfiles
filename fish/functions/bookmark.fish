de5e3f29bf3375be63602e7062d8c6932bf14bf9
Dan Motzenbecker <dan@oxism.com>
2016-12-23 11:42:57 -0500

    improved bookmark function

--- fish/functions/bookmark.fish
+++ fish/functions/bookmark.fish
@@ -1,4 +1,9 @@
 function bookmark
-  echo 'alias' $argv \''cd' (eval pwd)\' >> ~/.config/fish/bookmarks.fish
+  if count $argv > /dev/null
+    set n $argv
+  else
+    set n (basename (pwd))
+  end
+  echo 'alias' $n \''cd' (pwd)\' >> ~/.config/fish/bookmarks.fish
   . ~/.config/fish/bookmarks.fish
 end
