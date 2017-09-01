7ad35c6fc997f248e5de5101b3cdf0fabaa96f5e
Dan Motzenbecker <dan@oxism.com>
2017-09-01 13:56:29 -0400

    separate path additions

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,17 @@
 set    fish_greeting ''
 set    HISTSIZE      20000
-set -x EDITOR        atom
-set -x PATH          $PATH ~/homebrew/bin ~/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin ~/homebrew/anaconda3/bin/
+set -x PATH          $PATH \
+  ~/homebrew/bin \
+  ~/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin \
+  ~/homebrew/anaconda3/bin/
+
+
+set -x EDITOR        'atom'
 set -x NODE_ENV      'development'
 set -x NODE_PATH     '/usr/local/lib/node'
 set -x GREP_OPTIONS  '--color=always'
 set -x GREP_COLOR    '1;35;40'
-set -x GOPATH        ~/.go
+set -x GOPATH        '~/.go'
 
 set -x CUDA_HOME         /usr/local/cuda
 set -x DYLD_LIBRARY_PATH $DYLD_LIBRARY_PATH $CUDA_HOME/lib
