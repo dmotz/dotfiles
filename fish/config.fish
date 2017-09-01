0f7dcef4ba948d8af33104a0d415606600ac0b72
Dan Motzenbecker <dan@oxism.com>
2017-09-01 13:58:37 -0400

    config tidying

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,10 @@
-set    fish_greeting ''
-set    HISTSIZE      20000
-set -x PATH          $PATH \
+set -x PATH $PATH \
   ~/homebrew/bin \
   ~/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin \
   ~/homebrew/anaconda3/bin/
 
+set fish_greeting ''
+set HISTSIZE      100000
 
 set -x EDITOR        'atom'
 set -x NODE_ENV      'development'
