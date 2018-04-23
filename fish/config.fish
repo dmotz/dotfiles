ab1bfe80c6d67ee2a0b2e71e90c835ebe24a7fe4
Dan Motzenbecker <dan@oxism.com>
2018-04-23 13:53:58 -0400

    android env vars

--- fish/config.fish
+++ fish/config.fish
@@ -9,13 +9,16 @@ set -x PATH $PATH \
 set fish_greeting ''
 set HISTSIZE      100000
 
-set -x EDITOR        'atom'
-set -x NODE_ENV      'development'
-set -x NODE_PATH     '/usr/local/lib/node'
-set -x GREP_OPTIONS  '--color=always'
-set -x GREP_COLOR    '1;35;40'
-set -x GOPATH        '~/.go'
+set -x EDITOR            'atom'
+set -x NODE_ENV          'development'
+set -x GREP_OPTIONS      '--color=always'
+set -x GREP_COLOR        '1;35;40'
 
+set -x NODE_PATH         /usr/local/lib/node
+set -x GOPATH            ~/.go
+set -x ANDROID_HOME      ~/Library/Android/sdk
+set -x ANDROID_SDK_ROOT  ~/Library/Android/sdk
+set -x ANDROID_AVD_HOME  ~/.android/avd
 set -x CUDA_HOME         /usr/local/cuda
 set -x DYLD_LIBRARY_PATH $DYLD_LIBRARY_PATH $CUDA_HOME/lib
 set -x LD_LIBRARY_PATH   $LD_LIBRARY_PATH $CUDA_HOME/lib64
