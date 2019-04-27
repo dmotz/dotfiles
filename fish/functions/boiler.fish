cce27c7fd1d6b820ea04d5125cf5ef434bdfeb25
Dan Motzenbecker <dan@oxism.com>
2019-04-26 20:47:26 -0400

    updated html boiler

--- fish/functions/boiler.fish
+++ fish/functions/boiler.fish
@@ -1,19 +1,22 @@
 function boiler
-  echo '
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-  <meta name="author" content="Dan Motzenbecker">
-  <!-- <link href="images/favicon.png" rel="icon" type="image/png"> -->
-  <!-- <link rel="stylesheet" type="text/css"> -->
-  <title></title>
-</head>
-<body>
-  <script src=""></script>
-</body>
+  echo \
+"<!DOCTYPE html>
+<html lang=\"en\">
+  <head>
+    <meta charset=\"utf-8\" />
+    <meta name=\"description\" content=\"\" />
+    <meta
+      name=\"viewport\"
+      content=\"width=device-width, initial-scale=1.0, user-scalable=no\"
+    />
+    <meta name=\"author\" content=\"Dan Motzenbecker\" />
+    <!-- <link href=\"images/favicon.png\" rel=\"icon\" type=\"image/png\" /> -->
+    <!-- <link rel=\"stylesheet\" type=\"text/css\" /> -->
+    <title>$argv[1]</title>
+  </head>
+  <body>
+    <script src=\"src/index.js\"></script>
+  </body>
 </html>
-'
+"
 end
