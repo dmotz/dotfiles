687face46e53da0c28f898bd9bc97dd1a803de46
Dan Motzenbecker <dan@oxism.com>
2019-09-03 11:02:13 -0400

    updated lein profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,16 +1,14 @@
 {:user
  {:plugins
-  [[cider/cider-nrepl "0.20.0"]
+  [[cider/cider-nrepl "0.22.0"]
    [lein-ancient "0.6.15"]
-   [venantius/ultra "0.5.2"]
    [lein-cljfmt "0.6.4"]
-   [jonase/eastwood "0.3.5"]
-   [lein-bikeshed "0.5.1"]
+   [jonase/eastwood "0.3.6"]
+   [lein-bikeshed "0.5.2"]
    [lein-try "0.4.3"]
    [lein-exec "0.3.7"]
-   [lein-kibit "0.1.6"]]
+   [lein-kibit "0.1.7"]]
 
-  :middleware [cider-nrepl.plugin/middleware
-               ultra.plugin/middleware]
+  :middleware [cider-nrepl.plugin/middleware]
 
   :ultra {:color-scheme :solarized_dark}}}
