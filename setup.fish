c0417c07a4f2af1a8868ae099af24af2d2b32de8
Dan Motzenbecker <dan@oxism.com>
2019-12-01 13:27:56 -0500

    added conda symlink to setup script

--- setup.fish
+++ setup.fish
@@ -4,6 +4,7 @@ ln -s $CONFIG_PATH/js/.eslintrc ~/.eslintrc
 ln -s $CONFIG_PATH/js/.prettierrc ~/.prettierrc
 ln -s $CONFIG_PATH/.lein ~/.lein
 ln -s $CONFIG_PATH/git/.gitignore_global ~/.gitignore_global
+ln -s $CONFIG_PATH/conda/.condarc ~/.condarc
 ln -s $CONFIG_PATH/editors/vscode/settings.json \
       ~/Library/Application\ Support/Code/User/settings.json
 ln -s $CONFIG_PATH/editors/vscode/keybindings.json \
