f9cac421cbe14c6d965e62650272d1dae33833d5
Dan Motzenbecker <dan@oxism.com>
2020-04-16 13:29:07 -0400

    updated t to use notif

--- fish/functions/t.fish
+++ fish/functions/t.fish
@@ -1,5 +1,5 @@
 function t
   sleep (math "$argv[1] * 60")
-  osascript -e "display notification \"$argv[2..-1]\" with title \"👋\""
+  notif $argv[2..-1]
   afplay /System/Library/Sounds/Blow.aiff
 end
