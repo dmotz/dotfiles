cf1db7c263ca7b3adc8196f4c495747bd309bd82
Dan Motzenbecker <dan@oxism.com>
2021-01-19 22:27:51 -0500

    removed atom

--- setup.fish
+++ setup.fish
@@ -9,7 +9,7 @@ ln -s $CONFIG_PATH/editors/vscode/settings.json \
       ~/Library/Application\ Support/Code/User/settings.json
 ln -s $CONFIG_PATH/editors/vscode/keybindings.json \
       ~/Library/Application\ Support/Code/User/keybindings.json
-ln -s $CONFIG_PATH/editors/atom/config.cson ~/.atom/config.cson
+
 ln -s $CONFIG_PATH/clojure/deps.edn ~/.clojure/deps.edn
 ln -s $CONFIG_PATH/clojure/user.clj ~/.clojure/user.clj
 ln -s $CONFIG_PATH/stack/config.yaml ~/.stack/config.yaml
