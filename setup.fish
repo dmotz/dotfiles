6d339e02052e85ca0a79acba80bc82c618d61d78
Dan Motzenbecker <dan@oxism.com>
2021-02-28 16:37:25 -0500

    updated fisher install

--- setup.fish
+++ setup.fish
@@ -52,8 +52,5 @@ sudo echo $BREW_PATH/bin/fish >> /etc/shells
 chsh -s $BREW_PATH/bin/fish
 
 if not functions -q fisher
-  set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-  curl https://git.io/fisher --create-dirs -sLo \
-    $XDG_CONFIG_HOME/fish/functions/fisher.fish
-  fish -c fisher
+  curl -sL https://git.io/fisher | source; and fisher install jorgebucaran/fisher
 end
