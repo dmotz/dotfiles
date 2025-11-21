2aa9a26cd449cf85fa6d42ffc8eaa9187afa0a4c
Dan Motzenbecker <dan@oxism.com>
2025-11-20 23:32:58 -0500

    add branch name to fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,12 +1,27 @@
 function fish_prompt
   if test (pwd) = $HOME
     set_color -o green
-    echo '~'
+    echo -n '~'
   else
     echo -n (dirname (pwd) | sed "s|^$HOME|~|")/
     set_color -o brwhite
-    echo (basename (pwd))
+    echo -n (basename (pwd))
   end
+
+  set -l git_branch (git branch --show-current 2>/dev/null)
+
+  if test -z "$git_branch"
+    if git rev-parse --is-inside-work-tree >/dev/null 2>&1
+      set git_branch (git rev-parse --short HEAD 2>/dev/null)
+    end
+  end
+
+  if test -n "$git_branch"
+    set_color -o red
+    echo -n " ($git_branch)"
+  end
+
+  echo
 
   set_color -o blue
   echo -n '$ '
